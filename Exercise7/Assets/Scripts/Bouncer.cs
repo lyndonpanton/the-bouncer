0e5efa8b425e93568edb65b5e13b0c9d12a7159a
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-06 20:19:01 +0000

    Completed problem 2
    The game object now bounces around the screen and is destroyed once its health is equal to 0

---
+++ Exercise7/Assets/Scripts/Bouncer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    // the health of the game object
+    private int health = 100;
+
+
+    // the rigid body 2d component of the game object
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        int randomHorizontalInverse = 1;
+        int randomVerticalInverse = 1;
+
+        if (Random.Range(0, 2) == 1)
+        {
+            randomHorizontalInverse *= -1;
+        }
+
+        if (Random.Range(0, 2) == 1)
+        {
+            randomVerticalInverse *= -1;
+        }
+
+        float horizontalForce = Random.Range(1, 5) * randomHorizontalInverse;
+        float verticalForce = Random.Range(1, 5) * randomVerticalInverse;
+
+        rb2d.AddForce(
+            new Vector2(
+                horizontalForce, 
+                verticalForce
+            ),
+            ForceMode2D.Impulse
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health -= 10;
+    }
+}
